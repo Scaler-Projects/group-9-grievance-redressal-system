fc231ba66a20174b76278b8add3648302ead8a27
Demba Siby <dembasiby@gmail.com>
2023-10-26 16:43:02 +0000

    Created endpoint to retrieve user details

--- user-management-service/src/main/java/com/scaler/usermanagementservice/controllers/UserController.java
+++ user-management-service/src/main/java/com/scaler/usermanagementservice/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.scaler.usermanagementservice.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return new ResponseEntity<>(this.userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<UserDto> getUserDetails(@PathVariable("user_id") Long user_id) {
+        UserDto userDto = this.userService.getUserDetails(user_id);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 }
