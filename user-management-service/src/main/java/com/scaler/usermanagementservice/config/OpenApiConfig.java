a0c5308ae4776c9e45768ea5074e468efb059ad7
Demba Siby <dembasiby@gmail.com>
2023-10-28 01:04:41 +0000

    Added OpenApi

---
+++ user-management-service/src/main/java/com/scaler/usermanagementservice/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.scaler.usermanagementservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("User Management API")
+                        .description("This is a sample User Management Service")
+                        .version("1.0.0"));
+    }
+}
