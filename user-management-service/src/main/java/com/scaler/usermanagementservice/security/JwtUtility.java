e1326513bb624dcfcdc6d09876176991fefc5850
Demba Siby <dembasiby@gmail.com>
2023-11-01 12:50:00 +0000

    Automate the secret key generation

--- user-management-service/src/main/java/com/scaler/usermanagementservice/security/JwtUtility.java
+++ user-management-service/src/main/java/com/scaler/usermanagementservice/security/JwtUtility.java
@@ -8,16 +8,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.util.Base64;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 
 @Component
 public class JwtUtility {
     private static final int TOKEN_VALIDITY = 3600 * 5;
-    private static final String SECRET_KEY =  "This is a secret key This is a secret key   This is a secret key This is a secret keyThis is a secret key This is a secret key";
 
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
@@ -29,7 +25,9 @@ public class JwtUtility {
     }
 
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJwt(token).getBody();
+        return Jwts.parser()
+                .setSigningKey(generateSecretKey(SignatureAlgorithm.HS256))
+                .parseClaimsJwt(token).getBody();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
@@ -48,11 +46,11 @@ public class JwtUtility {
 
     public String generateToken(UserDetails userDetails) {
 
+
         SignatureAlgorithm alg = SignatureAlgorithm.HS256;
-        SecretKey key = Keys.secretKeyFor(alg);
+        SecretKey key = generateSecretKey(alg);
         Map<String, Object> claims = new HashMap<>();
 
-        claims.put("username", userDetails.getUsername());
         claims.put("roles", userDetails.getAuthorities());
 
         return Jwts.builder()
@@ -60,8 +58,12 @@ public class JwtUtility {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY * 1000))
-                .signWith(key, alg)
+                .signWith(generateSecretKey(SignatureAlgorithm.HS256), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public SecretKey generateSecretKey(SignatureAlgorithm signature) {
+        return Keys.secretKeyFor(signature);
     }
 
 }
